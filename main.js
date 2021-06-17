c53fb977a921b9320b22b2530c3c4f7eddce64d0
William Mabia <mabiawilliam@gmail.com>
2021-06-17 16:28:58 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,68 @@
+const TypeWriter = function (txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = "";
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+};
+
+// Type Method
+TypeWriter.prototype.type = function () {
+  // Current index of word
+  const current = this.wordIndex % this.words.length;
+
+  //Get full text of current word/index
+  const fullText = this.words[current];
+
+  //check if deleting
+  if (this.isDeleting) {
+    //Remove character
+    this.txt = fullText.substring(0, this.txt.length - 1);
+  } else {
+    //add character
+    this.txt = fullText.substring(0, this.txt.length + 1);
+  }
+
+  //Insert txt into element
+  this.txtElement.innerHTML = `<span class = "txt">${this.txt}</span>`;
+
+  //Type speed - want to be faster when deleting
+  let typeSpeed = 200;
+
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
+
+  // If word is complete
+  if (!this.isDeleting && this.txt === fullText) {
+    // Pause when done typing
+    typeSpeed = this.wait;
+
+    // Set delete to true
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    //Move to next word
+    this.wordIndex++;
+
+    //pause before restart typing
+    typeSpeed = 500;
+  }
+
+  setTimeout(() => this.type(), typeSpeed);
+};
+
+// Init on DOM load
+document.addEventListener("DOMContentLoaded", init);
+
+//Init App
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+
+  //Initialize typewriter
+  new TypeWriter(txtElement, words, wait);
+}
